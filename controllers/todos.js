bba035df229d843ac699f3eae42d70e2fe1cc2e0
edtroit <98837125+edtroit@users.noreply.github.com>
2022-09-05 22:26:31 -0400

    adding changes to todo.js controller to recognise users collection

---
+++ controllers/todos.js
@@ -0,0 +1,71 @@
+const Todo = require('../models/Todo')
+const User = require('../models/User')
+
+module.exports = {
+    getTodos: async (req,res)=>{
+        console.log(req.user)
+        try{
+            const todoItems = await Todo.find({userId:req.user.id})
+            const allUsers = await User.find()
+            const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
+            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user, users: allUsers})
+        }catch(err){
+            console.log(err)
+        }
+    },
+    createTodo: async (req, res)=>{
+        try{
+            await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id})
+            console.log('Todo has been added!')
+            res.redirect('/todos')
+        }catch(err){
+            console.log(err)
+        }
+    },
+    markComplete: async (req, res)=>{
+        try{
+            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+                completed: true
+            })
+            console.log('Marked Complete')
+            res.json('Marked Complete')
+        }catch(err){
+            console.log(err)
+        }
+    },
+    markIncomplete: async (req, res)=>{
+        try{
+            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+                completed: false
+            })
+            console.log('Marked Incomplete')
+            res.json('Marked Incomplete')
+        }catch(err){
+            console.log(err)
+        }
+    },
+    assignTodo: async (req, res)=>{
+        try{
+            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+                userId: {_id:req.body.updatedUserId}
+            })
+            console.log('Todo Assigned')
+            res.redirect('/todos')
+        }catch(err){
+            console.log(err)
+        }
+    },
+
+
+    deleteTodo: async (req, res)=>{
+        console.log(req.body.todoIdFromJSFile)
+        try{
+            await Todo.findOneAndDelete({_id:req.body.todoIdFromJSFile})
+            console.log('Deleted Todo')
+            res.json('Deleted It')
+            
+        }catch(err){
+            console.log(err)
+        }
+    }
+}    
